21cb19a2244594bb84723bd5989da0dd2a503fc9
pankona <yosuke.akatsuka@gmail.com>
2018-06-01 13:13:47 +0900

    add application name on top of page

--- main.go
+++ main.go
@@ -161,7 +161,7 @@ const index = `
 	</script>
 </head>
 <body>
-<div>Generate a phrase with 3 random words (version 0.1.0)</div>
+<div>PhraGen - Generate a phrase with 3 random words (version 0.1.0)</div>
 <br>
 <div>Concept</div>
 <li>Choice 3 words from English dictionary to generate strong password or passphrase.</li>
